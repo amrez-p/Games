dfca5d91e5ef450db36f6945d4e40510dd42e880
Lionliege <amrezpirzada2@gmail.com>
2021-10-03 04:18:31 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import ButtonMenu from "./components/ButtonMenu";
+import imageOne from "./components/Images/imageOne.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Two Towers */}
+      <div className="twoTowers">
+        {/* Tower Left */}
+        <div className="towerLeft">
+          <span className="towerLeft__container">
+            <img src={imageOne} alt="game" />
+            <img src={imageOne} alt="game" />
+          </span>
+          <span className="towerLeft__container">
+            <img src={imageOne} alt="game" />
+            <img src={imageOne} alt="game" />
+          </span>
+          <span className="towerLeft__container">
+            <img src={imageOne} alt="game" />
+            <img src={imageOne} alt="game" />
+          </span>
+        </div>
+        {/* Tower Left */}
+        {/* Tower Right */}
+        <div className="towerRight">
+          <span className="towerRight__container">
+            <img src={imageOne} alt="game" />
+            <img src={imageOne} alt="game" />
+          </span>
+          <span className="towerRight__container">
+            <img src={imageOne} alt="game" />
+            <img src={imageOne} alt="game" />
+          </span>
+          <span className="towerRight__container">
+            <img src={imageOne} alt="game" />
+            <img src={imageOne} alt="game" />
+          </span>
+          <span className="towerRight__container">
+            <img src={imageOne} alt="game" />
+            <img src={imageOne} alt="game" />
+          </span>
+          <span className="towerRight__container">
+            <img src={imageOne} alt="game" />
+            <img src={imageOne} alt="game" />
+          </span>
+        </div>
+        {/* Tower Right */}
+      </div>
+      {/* Two Towers */}
+
+      {/* Menu Buttons */}
+      <ButtonMenu />
+      {/* Menu Buttons */}
     </div>
   );
 }
---
+++ src/components/ButtonMenu.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const ButtonMenu = () => {
+  return (
+    <div className="menuButtons">
+      {/* Menu Border */}
+      <div className="menuBorder"></div>
+      {/* Menu Border */}
+
+      {/* menu Section */}
+      <div className="menuSection">
+        <button>save</button>
+        <button>load</button>
+        <span>...</span>
+        <button>(button)</button>
+        <button>(button)</button>
+        <button>(button)</button>
+        <button>(button)</button>
+        <button>(button)</button>
+        <button>(button)</button>
+        <button>(button)</button>
+        <span>...</span>
+      </div>
+      {/* menu Section */}
+    </div>
+  );
+};
+
+export default ButtonMenu;
